743e4267430753abf96077e20f323b98ecf51be2
Tesfu Tekleab <taffletekleab@gmail.com>
2023-06-15 16:34:04 -0700

    This is the first c program today

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Print a string using one of the c programming buitl in function puts.
+ *Return: The program should return 0 as per the question instruction.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
