780f8e0aabe9cfcd77dd9ba30485f9f6a2948ffc
Tesfu Tekleab <taffletekleab@gmail.com>
2023-06-15 16:48:50 -0700

    printf updates two

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  *main - Print a string using one of the c programming built in function printf.
- *Return - The program should return 0 (success) as per the instruction.
+ *Return: The program should return 0 (success) as per the instruction.
  */
 
 int main(void)
