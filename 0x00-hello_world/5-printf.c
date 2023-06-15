fff67c28581a5bc44778f23deab096e1b4088c57
Tesfu Tekleab <taffletekleab@gmail.com>
2023-06-15 16:42:04 -0700

    using the printf builtin function to print some string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Print a string using one of the c programming built in function printf.
+ *Return: The program should return 0 (success) as per the instruction.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
