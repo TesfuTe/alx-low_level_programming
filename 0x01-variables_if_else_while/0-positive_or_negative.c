72cab86cd83346a01bee05d4629bf96dc10337e6
Tesfu Tekleab <taffletekleab@gmail.com>
2023-06-16 01:14:19 -0700

    0. Positive anything is better than negative nothing indentation fixes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,8 +14,8 @@ int main(void)
 	    printf("%d is positive\n", n);
 	else if(n==0)
 	    printf("%d is zero",n)
-    else
-        printf("%d is negative",n)
+        else
+            printf("%d is negative",n)
         
 	return (0);
 }
