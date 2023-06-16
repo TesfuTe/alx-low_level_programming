a3a654d070b7f1b1be74f77d878ca02e54ebf411
Tesfu Tekleab <taffletekleab@gmail.com>
2023-06-16 03:42:30 -0700

    updates on +ve or -ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - this is checking the number for +ve -ve and zero.
+ * Return: This returns 0 (success) as per the instruction.
+ */
+
 int main(void)
 {
 	int n;
@@ -10,12 +13,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	if(n>0)
+	if(n > 0)
+	{
 	    printf("%d is positive\n", n);
-	else if(n==0)
-	    printf("%d is zero",n)
-        else
-            printf("%d is negative",n)
+	}
+	else if(n == 0)
+	{
+	    printf("%d is zero\n",n)
+	}
+       	else if(n < 0)
+	{
+	    printf("%d is negative\n",n)
+	}
         
 	return (0);
 }
