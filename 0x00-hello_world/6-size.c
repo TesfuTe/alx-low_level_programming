97f9ecf02a7a212aaf74b8ba69e7c716c6988041
Tesfu Tekleab <taffletekleab@gmail.com>
2023-06-15 17:05:53 -0700

    sizeof function to print data types length

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Print size of c programming data types using sizeof.
+ *Return: retuns 0 (success) as per the insturction.
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a char: %zu byte(s)\n", sizeof(unsigned char));
+	printf("Size of a char: %zu byte(s)\n", sizeof(short));
+	printf("Size of a char: %zu byte(s)\n", sizeof(unsigned short));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
