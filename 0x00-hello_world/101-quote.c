430dc5fdefc70b2cb87cbf5672f3fb2589742141
Tesfu Tekleab <taffletekleab@gmail.com>
2023-06-15 17:16:04 -0700

    advanced tasks done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - Using write to print string.
+ *Return: This returns 1 as per the instruction
+ *
+ */
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
